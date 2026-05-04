0cd861bae47c6bfa2060c01be897f03c3f122ae5
Param Preet Singh <parampreet.iot@gmail.com>
2026-05-04 20:02:31 +0530

    Day 1

---
+++ Workshop Projects/1.  Report Card Printer/main.py
@@ -0,0 +1,14 @@
+name = "Param"
+print(name, type(name))
+
+is_student = True
+print(is_student , type(is_student))
+
+age = 17
+print(age , type(age))
+
+score = 87.5
+print(score, type(score))
+
+Year = "2nd"
+print(Year, type(Year))
