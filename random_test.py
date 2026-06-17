ae9a9c4ceb0ccf1a13aa0286ced17614441140f8
Param Preet Singh <parampreet.iot@gmail.com>
2026-06-17 19:49:15 +0530

    Update random_test.py

--- random_test.py
+++ random_test.py
@@ -8,19 +8,3 @@ Param = {
     "Waifu": "Rem",
 
 }
-# no = int(input("Enter a number: "))
-# till = int(input("Enter a number till which you want to print the table: "))
-# for i in range(1, till+1):
-#     print(f"{no} X {i}={no*i}")
-star = "*"
-layer  = int(input("Enter the number of layers: ")) + 2
-space = " "
-for i in range(1,layer+1):
-    if i==1 or i==layer:
-        print("*"*layer,end="")
-        print("")
-    else:
-        print(star,end="")
-        print(space*(layer-2),end="")
-        print(star,end="")
-        print("")
