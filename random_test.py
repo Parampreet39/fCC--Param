502c9e74efa9a030b202cf8a9e52e132f5a6a4d0
Param Preet Singh <parampreet.iot@gmail.com>
2026-06-19 20:54:34 +0530

    Reccursion very hard

--- random_test.py
+++ random_test.py
@@ -8,6 +8,35 @@ Param = {
     "Waifu": "Rem",
 
 }
-def greet(name):
-    print(f"Good Day {name}")
-greet("Rem")
+def greatest(a,b,c):
+    no = [a,b,c]
+    result = sorted(no,reverse=True)
+    print(sorted)
+    print(f"Greatest no. is {result[0]}")
+def sum_numbers(n):
+    if n == 1:
+        return 1  
+    return n + sum_numbers(n - 1)  
+def pattern(n):
+    if n==1:
+        print("*")
+        return 
+    if n != 0:
+        print(n*"*")
+        pattern(n-1)
+        return 
+    return
+def pattern(n):
+    if n == 0:
+        return
+        
+    pattern(n - 1)  # 👈 Call first (goes down to 0)
+    print(n * "*")  # 👈 Print last (prints on the way back up)
+
+pattern(4)
+
+"""
+*
+**
+***
+"""
