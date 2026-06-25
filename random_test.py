d8f17f9a4d5c401335248ce338f5cc0ac35d2701
Param Preet Singh <parampreet.iot@gmail.com>
2026-06-25 16:48:09 +0530

    I see

--- random_test.py
+++ random_test.py
@@ -8,35 +8,15 @@ Param = {
     "Waifu": "Rem",
 
 }
-def greatest(a,b,c):
-    no = [a,b,c]
-    result = sorted(no,reverse=True)
-    print(sorted)
-    print(f"Greatest no. is {result[0]}")
-def sum_numbers(n):
-    if n == 1:
-        return 1  
-    return n + sum_numbers(n - 1)  
-def pattern(n):
-    if n==1:
-        print("*")
-        return 
-    if n != 0:
-        print(n*"*")
-        pattern(n-1)
-        return 
-    return
-def pattern(n):
-    if n == 0:
-        return
-        
-    pattern(n - 1)  # 👈 Call first (goes down to 0)
-    print(n * "*")  # 👈 Print last (prints on the way back up)
-
-pattern(4)
-
 """
-*
-**
-***
+Python I/O
 """
+x = open("File.txt")
+message = x.read()
+print(message)
+y = open("File.txt", "w")
+name = input("Enter Your name")
+new_message = name
+y.write(new_message)
+print(y)
+y.close()
